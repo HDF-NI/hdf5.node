493577e1daeffd5b95e9b6163d0addd740b35414
Ryan Cole <ryan@rycole.com>
2012-04-23 16:07:36 -0500

    output file object

--- test/hdf5.js
+++ test/hdf5.js
@@ -5,6 +5,7 @@ var hdf5 = require('../build/Release/hdf5');
 // instantiate h5file
 var h5file = new hdf5.H5File('/home/ryan/data/MillionSongSubset/data/A/A/A/TRAAAAW128F429D538.h5');
 
+console.log(h5file);
 console.log(h5file.filename());
 console.log(h5file.filesize());
 
