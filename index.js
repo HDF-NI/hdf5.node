758f6a8e9cdda0f4d04c678da0ee75b3640d7c0c
roger <roger@roger-G74Sx>
2014-12-05 10:19:27 -0500

    Fixed module export to avoid undefine

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var hdf5;
+var hdf5=exports;
 
 hdf5.hdf5 = require ("./build/Release/obj.target/hdf5");
 hdf5.h5lt = require ("./build/Release/obj.target/h5lt");
@@ -7,4 +7,4 @@ hdf5.h5tb = require ("./build/Release/obj.target/h5tb");
 hdf5.h5im = require ("./build/Release/obj.target/h5im");
 
 console.dir("hdf5 exports");
-module.exports=exports=hdf5;
+module.exports=hdf5;
