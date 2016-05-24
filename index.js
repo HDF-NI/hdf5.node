e88e2ba0c23e62427a0c9f1e2a47dd9b5d4ec823
Gabriel Pichot <gabriel.pichot@gmail.com>
2016-05-24 15:21:52 -0700

    fixup! Adds support for UNLIMITED Dataset and dynamic addition of variable length string

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 for (let mod of ["hdf5", "h5lt", "h5tb", "h5pt", "h5im", "h5ds"]) {
-  module.exports[mod] = require(`./build/Debug/${mod}`);
+  module.exports[mod] = require(`./build/Release/${mod}`);
 }
