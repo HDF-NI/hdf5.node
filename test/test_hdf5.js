2d8d630a9a77adf15c315a078a80aacd7b89c30d
roger <mathmldashx@yahoo.com>
2016-08-20 13:57:12 -0400

    testing getMemberNames and the common interface

--- test/test_hdf5.js
+++ test/test_hdf5.js
@@ -15,48 +15,56 @@ describe("testing c interface ", function() {
           file = new hdf5Lib.hdf5.File('./TRAAAAW128F429D538.h5', globs.Access.ACC_TRUNC);
         });
 
-        it("should be >0 ", function*() {
-            const group=file.createGroup('/pmcservices/x-ray/refinement');
+        it("should be >0 ", function () {
+            const group=file.createGroup('/pmc/x-ray/refinement');
             group.id.should.not.equal(-1);
             group.close();
         });
 
-        it("reopen of pmc should be >0", function*() {
+        it("reopen of pmc should be >0", function() {
             const groupPmc=file.openGroup('pmc');
             groupPmc.id.should.not.equal(-1);
             groupPmc.close();
         });
 
-        it("should be >0 ", function*() {
+        it("should be >0 ", function () {
             const xpathGroup=file.createGroup('pmc/Trajectories');
             xpathGroup.id.should.not.equal(-1);
             xpathGroup.close();
         });
 
-        it("initial should be >0 ", function*() {
+        it("initial should be >0 ", function () {
             const xpathGroup=file.createGroup('pmc/Trajectories/0');
             xpathGroup.id.should.not.equal(-1);
             xpathGroup.close();
         });
 
-        it("move should be 1 ", function*() {
+        it("move should be 1 ", function () {
             const stemGroup=file.createGroup('pmc/Trajectories');
             stemGroup.move("0", stemGroup.id, "1");
             stemGroup.close();
         });
 
-        it("move should be pmcservices ", function*() {
+        it("move should be pmcservices ", function () {
             file.move("pmc", file.id, "pmcservices");
         });
 
-        it("should have one child of type group ", function*() {
+        it("should have one child of type group ", function () {
             const group=file.openGroup('pmcservices');
-            group.getNumObjs().should.equal(1);
+            group.getNumObjs().should.equal(2);
             group.getChildType("Trajectories").should.equal(globs.H5OType.H5O_TYPE_GROUP);
             group.close();
         });
 
-        it("should add an attribute to the file ", function*() {
+        it("should get member names ", function () {
+            const names=file.getMemberNames();
+            names.length.should.equal(1);
+            names.forEach(function(val) {
+                console.dir(val);
+            });
+        });
+
+        it("should add an attribute to the file ", function () {
             file.role="Target";
             file.flush();
         });
