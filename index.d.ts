55550e2e229b6c3229be0ed06f98e56694a737ef
Christian Nienhaus <c.nienhaus@canway.de>
2018-05-16 10:36:03 +0200

    Add type definitions.

---
+++ index.d.ts
@@ -0,0 +1,364 @@
+// Type definitions for hdf5 0.3
+// Project: https://github.com/HDF-NI/hdf5.node#readme
+// Definitions by: NINI1988 <https://github.com/NINI1988>
+
+import { Access, H5OType, HLType, H5Type, H5TOrder, H5SType } from "hdf5/lib/globals";
+
+/// <reference types="node" />
+
+type TypedArray = Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array;
+
+export declare class Hdf5Buffer extends Buffer {
+    rank?: number;
+    type?: H5Type;
+
+    /**
+     * dimension 1
+     */
+    rows?: number;
+
+    /**
+     * dimension 2
+     */
+    columns?: number;
+
+    /**
+     * dimension 3
+     */
+    sections?: number;
+
+    /**
+     * dimension 4
+     */
+    files?: number;
+}
+
+export declare namespace hdf5 {
+
+    class Methods {
+
+        public id: Int64;
+
+        /**
+         * 
+         * @param xpath  the path to the group to open. Delimited with / and will throw an exception if it doesn’t exist. Useful particularly in read only mode
+         * @returns the opened group object
+         */
+        public openGroup(xpath: string): Group;
+
+        /**
+         * Refreshes subtree and metadata from h5 file to h5 components in memory and on to the javascript. Reverse action to flush.
+         */
+        public refresh(): void;
+
+        /**
+         * 
+         * @param source the name of the source group
+         * @param id the group or file id location. Every group and the file return with an id attribute. In this case it is the destination’s future location. If it is the file’s id this essentially renames the source
+         * @param destination the name of the destination group.
+         */
+        public move(source: string, id: Int64, destination: string): any;
+
+        /**
+         * @returns # of attributes
+         */
+        public getNumAttrs(): number;
+
+        /**
+         * 
+         * @param name  the name of the group to delete. An apprehensive activity with an h5 file but sometimes it is necessary
+         */
+        public delete(name: string): void;
+
+        /**
+         * Flushes subtree and metadata from javascript to h5 components in memory and on to the h5 file.
+         */
+        public flush(): void;
+
+        public close(): void;
+
+        /**
+         * @returns # of children
+         */
+        public getNumObjs(): number;
+
+        /**
+         * @returns an array with the names in alphabetic order
+         */
+        public getMemberNames(): string[];
+
+        /**
+         * @returns an array with the names in creation order
+         */
+        public getMemberNamesByCreationOrder(): string[];
+
+        /**
+         * @param name the name of the child
+         * @returns the type according to H5OType and indicates group, dataset or named datatype
+         */
+        public getChildType(name: string): H5OType;
+
+        /**
+         * @param name the name of the child dataset
+         * @returns the high level type according to HLType and indicates group, dataset or named datatype. Useful when working with h5 in general and programmatically would like to know which part of the api to use
+         */
+        public getDatasetType(name: string): HLType;
+
+        /**
+         * 
+         * @param name the name of the child dataset
+         * @returns an array of length dataset rank and entries for the length of each dimension
+         */
+        public getDatasetDimensions(name: string): number[];
+
+        /**
+         * 
+         * @param name the name of the child
+         * @returns the type according to H5Type. Useful when working with h5 in general and programmatically would like to prepare for particular data type
+         */
+        public getDataType(name: string): H5Type;
+
+        /**
+         * 
+         * @param name the name of the child dataset
+         * @returns object with attributes
+         */
+        public getDatasetAttributes(name: string): object;
+
+        /**
+         * 
+         * @param name 
+         * @returns a Filters object for accessing the dataset’s filters
+         */
+        public getFilters(name: string): Filters;
+
+        /**
+         * 
+         * @param idx unused starting over index
+         * @param callback synchronous callback function returning H5Type and name
+         */
+        // file.iterate is not a function
+        // public iterate(idx: number, callback: (type: H5Type, name: string) => void): void;
+
+        /**
+         * 
+         * @param idx unused starting over index
+         * @param callback synchronous callback function returning H5Type and xpath
+         */
+        // file.visit is not a function
+        // public visit(idx: number, callback: (type: H5Type, xpath: string) => void): void;
+    }
+
+    class File extends Methods {
+
+        constructor(filename: string, access: Access);
+
+        /**
+         * Turn on singe write, multi read mode
+         */
+        public enableSingleWriteMumltiRead(): void;
+
+        /**
+         * 
+         * @param xpath the path to the group to create. Delimited with / and if any group along the stem doesn’t exist creates it too
+         * @returns the new group object
+         */
+        public createGroup(xpath: string): Group;
+
+        /**
+         * 
+         * @param name the name of the child dataset
+         */
+        public getByteOrder(name: string): H5TOrder;
+
+    }
+
+    class Int64 {
+
+    }
+
+    class Group extends Methods {
+
+        /**
+         * 
+         * @param xpath the path to the group to create. Delimited with / and if any group along the stem doesn’t exist creates it too
+         * @returns the new group object
+         */
+        public create(xpath: string): Group;
+
+        /**
+         * 
+         * @param xpath  the path to the group to open. Delimited with / and will throw an exception if it doesn’t exist. Useful particularly in read only mode
+         * @returns the opened group object
+         */
+        public open(xpath: string): Group;
+
+        /**
+         * 
+         * @param source the name of the source group
+         * @param id the group or file id location. Every group and the file return with an id attribute. In this case it is the destination’s future location
+         * @param destination the name of the destination group
+         */
+        public copy(source: string, id: hdf5.Int64, destination: string): void;
+
+        /**
+         * 
+         * @param source the name of the source group
+         * @param id the group or file id location.Every group and the file return with an id attribute.In this case it is the destination’s future location
+         * @param destination the name of the destination group
+         */
+        public link(source: string, id: hdf5.Int64, destination: string): void;
+
+    }
+
+    class Filters {
+
+    }
+
+    function getLibVersion(): string;
+
+}
+
+export declare namespace h5lt {
+
+    interface MakeDatasetOptions {
+
+        /**
+         * 0 and 9 inclusive. 0 is the lowest compression, but is the fastest. 9 gives the best compression, but is the slowest
+         */
+        compression?: number;
+        fixed_width?: number;
+        chunkSize?: number;
+
+        maxRows?: number | H5SType;
+        maxColumns?: number | H5SType;
+        maxSections?: number | H5SType;
+        maxFiles?: number | H5SType;
+    }
+
+    interface DatasetOptions {
+        start: number[];
+
+        /**
+         * The stride to step over in each dimension
+         */
+        stride: number[];
+        count: number[];
+    }
+
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset
+     * @param buffer Typed array, string or a node::Buffer. The buffer can have attributes describing its shape. When using a node::Buffer the data is expected to be homogeneous and the type attribute set to choice in H5Type
+     */
+    function makeDataset(id: hdf5.Int64, name: string, buffer: TypedArray | string | Hdf5Buffer): void;
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset
+     * @param buffer Typed array, string or a node::Buffer. The buffer can have attributes describing its shape. When using a node::Buffer the data is expected to be homogeneous and the type attribute set to choice in H5Type
+     * @param options currently has the option to set compression { compression: 6}. can be 0 through 9. For an Array of fixed width strings {fixed_width: maxLength} option sets the width. The Array can hold variable length strings and this call will make a fixed buffer and if any are actually greater than maxLength an exception is thrown.
+     */
+    function makeDataset(id: hdf5.Int64, name: string, buffer: TypedArray | string | Hdf5Buffer, options: MakeDatasetOptions): void;
+
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset
+     * @param buffer Typed array, string or a node::Buffer. The buffer can have attributes describing its shape. When using a node::Buffer the data is expected to be homogeneous and the type attribute set to choice in H5Type
+     */
+    function writeDataset(id: hdf5.Int64, name: string, buffer: TypedArray | string | Hdf5Buffer): void;
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset
+     * @param buffer Typed array, string or a node::Buffer. The buffer can have attributes describing its shape. When using a node::Buffer the data is expected to be homogeneous and the type attribute set to choice in H5Type
+     * @param options optional: start, stride and count
+     */
+    function writeDataset(id: hdf5.Int64, name: string, buffer: string | Hdf5Buffer, options: DatasetOptions): void;
+
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset
+     * @returns A typed array or string
+     */
+    function readDataset(id: hdf5.Int64, name: string): TypedArray | string;
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset
+     * @param options start, stride and count
+     * @returns A typed array or string
+     */
+    function readDataset(id: hdf5.Int64, name: string, options: DatasetOptions): TypedArray | string;
+
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset.
+     * @return A node::Buffer with the data and attributes for its shape and type
+     */
+    function readDatasetAsBuffer(id: hdf5.Int64, name: string): Hdf5Buffer;
+    /**
+     * 
+     * @param id the group or file id location. Every group and the file return with an id attribute
+     * @param name a string naming the dataset.
+     * @param options start, stride and count
+     * @return A node::Buffer with the data and attributes for its shape and type
+     */
+    function readDatasetAsBuffer(id: hdf5.Int64, name: string, options: DatasetOptions): Hdf5Buffer;
+
+    function readDatasetDatatype(): any;
+    function readDatasetLength(): any;
+}
+
+export interface h5ds {
+    attachScale: any;
+    detachScale: any;
+    getLabel: any;
+    getNumberOfScales: any;
+    getScaleName: any;
+    isAttached: any;
+    isScale: any;
+    iterateScale: any;
+    setLabel: any;
+    setScale: any;
+}
+
+export interface h5im {
+    getImageInfo: any;
+    isImage: any;
+    makeImage: any;
+    makePalette: any;
+    readImage: any;
+    readImageRegion: any;
+}
+
+export interface h5pt {
+    Int64: any;
+    PacketTable: any;
+    makeTable: any;
+    readTable: any;
+}
+
+export interface h5tb {
+    addRecordsFrom: any;
+    appendRecords: any;
+    combineTables: any;
+    deleteField: any;
+    deleteRecord: any;
+    getFieldInfo: any;
+    getTableInfo: any;
+    insertField: any;
+    insertRecord: any;
+    makeTable: any;
+    readFieldsIndex: any;
+    readFieldsName: any;
+    readRecords: any;
+    readTable: any;
+    writeFieldsIndex: any;
+    writeFieldsName: any;
+    writeRecords: any;
+}
---
+++ lib/globals.d.ts
@@ -0,0 +1,139 @@
+// Type definitions for hdf5 0.3
+// Project: https://github.com/HDF-NI/hdf5.node#readme
+// Definitions by: NINI1988 <https://github.com/NINI1988>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export declare enum Access {
+    /** absence of rdwr => rd-only */
+    ACC_RDONLY,
+    /** open for read and write    */
+    ACC_RDWR,
+    /** overwrite existing files   */
+    ACC_TRUNC,
+    /** fail if file already exists*/
+    ACC_EXCL,
+    /** print debug info      */
+    ACC_DEBUG,
+    /** create non-existing files  */
+    ACC_CREAT
+}
+
+export declare enum CreationOrder {
+    H5P_CRT_ORDER_TRACKED,
+    H5P_CRT_ORDER_INDEXED
+}
+
+export declare enum State {
+    COUNT,
+    TITLE,
+    DATA
+}
+
+export declare enum H5SType {
+    H5S_UNLIMITED = -1
+}
+
+export declare enum HLType {
+    HL_TYPE_UNKNOWN, /* Unknown object type                      */
+    HL_TYPE_LITE,  /* Lite dataset interface                        */
+    HL_TYPE_IMAGE,  /* Image                      */
+    HL_TYPE_TABLE,  /* Table   */
+    HL_TYPE_PACKET_TABLE,  /* Packets       */
+    HL_TYPE_TEXT,
+    HL_TYPE_DIMENSION_SCALES,  /*        */
+    HL_TYPE_OPTIMIZED_FUNCTIONS,  /*        */
+    HL_TYPE_EXTENSIONS   /*        */
+}
+
+export declare enum H5OType {
+    H5O_TYPE_UNKNOWN, /* Unknown object type                      */
+    H5O_TYPE_GROUP,  /* Object is a group                        */
+    H5O_TYPE_DATASET,  /* Object is a dataset                      */
+    H5O_TYPE_NAMED_DATATYPE,  /* Object is a committed (named) datatype   */
+    H5O_TYPE_NTYPES   /* Number of different object types (must   */
+    /* be last!)                                */
+}
+
+export declare enum H5TOrder {
+    H5T_ORDER_LE,  /* Little-endian byte order */
+    H5T_ORDER_BE, /*  Big-endian byte order */
+    H5T_ORDER_VAX, /* VAX mixed byte order  */
+    H5T_ORDER_MIXED, /* Mixed byte order among members of a compound datatype (see below)  */
+    H5T_ORDER_NONE, /* No particular order (fixed-length strings, object and region references) */
+}
+
+export declare enum H5ZType {
+    H5Z_FILTER_ALL,       //Removes all filters from the filter pipeline.
+    H5Z_FILTER_DEFLATE, //Data compression filter, employing the gzip algorithm
+    H5Z_FILTER_SHUFFLE, //Data shuffling filter
+    H5Z_FILTER_FLETCHER32, //Error detection filter, employing the Fletcher32 checksum algorithm
+    H5Z_FILTER_SZIP, //Data compression filter, employing the SZIP algorithm
+    H5Z_FILTER_NBIT, //Data compression filter, employing the N-Bit algorithm
+    H5Z_FILTER_SCALEOFFSET  //Data compression filter, employing the scale-offset algorithm
+}
+
+export declare enum H5Type {
+    H5T_STD_I8BE,
+    H5T_STD_I8LE,
+    H5T_STD_I16BE,
+    H5T_STD_I16LE,
+    H5T_STD_I32BE,
+    H5T_STD_I32LE,
+    H5T_STD_I64BE,
+    H5T_STD_I64LE,
+    H5T_STD_U8BE,
+    H5T_STD_U8LE,
+    H5T_STD_U16BE,
+    H5T_STD_U16LE,
+    H5T_STD_U32BE,
+    H5T_STD_U32LE,
+    H5T_STD_U64BE,
+    H5T_STD_U64LE,
+    H5T_NATIVE_CHAR,
+    H5T_NATIVE_SCHAR,
+    H5T_NATIVE_UCHAR,
+    H5T_NATIVE_SHORT,
+    H5T_NATIVE_USHORT,
+    H5T_NATIVE_INT,
+    H5T_NATIVE_UINT,
+    H5T_NATIVE_LONG,
+    H5T_NATIVE_ULONG,
+    H5T_NATIVE_LLONG,
+    H5T_NATIVE_ULLONG,
+    H5T_IEEE_F32BE,
+    H5T_IEEE_F32LE,
+    H5T_IEEE_F64BE,
+    H5T_IEEE_F64LE,
+    H5T_NATIVE_FLOAT,
+    H5T_NATIVE_DOUBLE,
+    H5T_NATIVE_LDOUBLE,
+    H5T_STRING,
+    NODE_STRSIZE,
+    NODE_STRPAD,
+    NODE_CSET,
+    NODE_CTYPE,
+    H5T_VARIABLE,
+    H5T_STR_NULLTERM,
+    H5T_STR_NULLPAD,
+    H5T_STR_SPACEPAD,
+    H5T_CSET_ASCII,
+    H5T_CSET_UTF8,
+    H5T_C_S1,
+    H5T_FORTRAN_S1,
+    H5T_OPAQUE,
+    NODE_OPQ_SIZE,
+    NODE_OPQ_TAG,
+    H5T_COMPOUND,
+    H5T_ENUM,
+    H5T_ARRAY,
+    H5T_VLEN,
+    NODE_STRING,
+    NODE_NUMBER,
+    H5T_NATIVE_INT8,
+    H5T_NATIVE_UINT8,
+}
+
+export declare enum Interlace {
+    INTERLACE_PIXEL,
+    INTERLACE_PLANE
+}
